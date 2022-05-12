071a91f8061719142199d7ede9077c03e7378222
TiKevin83 <travismcgeehan@gmail.com>
2022-05-12 00:20:13 -0400

    Update to Next 12

---
+++ docs/_next/static/4rZCtsgreFanCANaoHvQZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/871-d5a9ee3fdc7823d5.js","static/chunks/pages/index-279554d6cce50796.js"],"/_error":["static/chunks/pages/_error-635a6533d6ac3ca1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
